c873e90878f4633553c95211c85a4c429757ee9d
Santo Sidauruk <santosidauruk@gmail.com>
2023-01-09 22:24:49 +0700

    style: slicing input component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -12,17 +12,23 @@ const Content = () => {
         <h3 className="px-9 py-8 text-2xl font-bold text-neutral-800 border-b border-neutral-400">
           Kalkulator Investasi Bulanan & Tahunan
         </h3>
-        <form action="" className="py-9">
+        <form action="" className="p-9">
           <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            {/* TODO:
+            - Add dynamic filled width if the content inside prefix changes
+            - Fix focus state on input */}
+            <div className="absolute inset-y-0 left-0 flex items-center p-3 pointer-events-none font-bold border-purple-300 bg-neutral-200 border border-r rounded-l-lg">
               Rp
             </div>
             <input
               type="text"
               id="email-address-icon"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="border border-purple-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-14 pr-14 p-2.5"
               placeholder="name@flowbite.com"
             />
+            <div className="absolute inset-y-0 right-0 flex items-center p-3 pointer-events-none font-bold border-purple-300 bg-neutral-200 border border-l rounded-r-lg">
+              %/Tahun
+            </div>
           </div>
         </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,20 @@ module.exports = {
       ...colors,
       purple: {
         ...colors.purple,
-        900: '#1C1B36',
-        700: '#323073',
-        500: '#6A67CB',
-        300: '#C3C2EA',
-        200: '#E3E3F0',
-        100: '#EEEEFA',
+        900: '#1C1B36', // Purple-Massive
+        700: '#323073', // Purple-Heavy
+        500: '#6A67CB', // Purple-Fair
+        400: '#C3C2EA', // Purple-Soft
+        300: '#CFD4ED', // Other/stroke
+        200: '#E3E3F0', // Purple-Root
+        100: '#EEEEFA', // Purple-Base
       },
       neutral: {
         ...colors.neutral,
-        800: '#333646',
-        600: '#656B8B',
-        400: '#B5B8C9',
-        200: '#F0F1F4',
+        800: '#333646', // Black-Fair
+        600: '#656B8B', // Black-Soft
+        400: '#B5B8C9', // Other/Divider
+        200: '#F0F1F4', // White-Soft
       },
     },
     extend: {},
