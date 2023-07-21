bc645593750db14d922082488d5aa12d380e020a
Santo Sidauruk <santosidauruk@gmail.com>
2023-07-21 19:11:28 +0700

    chore: update readme and meta

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Anggar.in
 ![GitHub deployments](https://img.shields.io/github/deployments/santosidauruk/anggar.in/production?logo=vercel&label=vercel&link=https%3A%2F%2Fgithub.com%2Fsantosidauruk%2Fanggar.in%2Fdeployments%2Factivity_log%3Fenvironment%3DProduction)
-<!-- asda -->
 
 ## Tech Stack
 - Next.js
@@ -8,11 +7,22 @@
 - Typescript
 
 ## Features
-- Calculate
+- Estimate how much funds you have after investing for some time. 
+- Estimate how long it will take you to invest to reach your goal.
+- Estimate how much funds you have to invest for each month or year.
+
 ## Screenshots
+### Final Investment Calculator
+![final investment calculator](public/ss_final_investment.png)
+
+### Investment Period Calculator
+![investment period calculator](public/ss_period_investment.png)
+
+### Periodic Investment Calculator
+![periodic investment calculator](public/ss_saving_investment.png)
 
 ## Contributions
-Contributions of any kind welcome!
+Contributions of any kind are welcome!
 
 ## Credits
 - [ipotgo](https://www.indopremier.com/ipotfund/calc-savinggoal.php)
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,55 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Anggar.in</title>
-        <meta name="description" content="Hitung mimpimu dengan anggar.in " />
+        <title>anggar.in - hitung potensi investasi kamu!</title>
+        <meta
+          name="title"
+          content="anggar.in - hitung potensi investasi kamu!"
+        />
+        <meta
+          name="description"
+          content="Anggar.in bantu kamu memperkirakan dana yang akan kamu miliki, berapa dana yang harus kamu investasi dan kapan kamu akan mencapai mimpimu"
+        />
+        <meta
+          name="keywords"
+          content="investasi, dana masa depan, kalkulator investasi"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          http-equiv="Content-Type"
+          content="text/html; charset=utf-8"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.anggar.in" />
+        <meta
+          property="og:title"
+          content="anggar.in - hitung potensi investasi kamu!"
+        />
+        <meta
+          property="og:description"
+          content="Anggar.in bantu kamu memperkirakan dana yang akan kamu miliki, berapa dana yang harus kamu investasi dan kapan kamu akan mencapai mimpimu"
+        />
+        <meta
+          property="og:image"
+          content="https://www.anggar.in/meta-image.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.anggar.in" />
+        <meta
+          property="twitter:title"
+          content="anggar.in - hitung potensi investasi kamu!"
+        />
+        <meta
+          property="twitter:description"
+          content="Anggar.in bantu kamu memperkirakan dana yang akan kamu miliki, berapa dana yang harus kamu investasi dan kapan kamu akan mencapai mimpimu"
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.anggar.in/meta-image.png"
+        />
       </Head>
 
       <main className="mx-4 md:mx-24">
